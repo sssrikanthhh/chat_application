4b67afc9b231bd0cdf6e4a4138c8a3ff9c12c427
srikanth <chiramanasrikanth265@gmail.com>
2023-01-25 20:49:53 +0530

    user model created

---
+++ api/models/userModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const userModel = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 20
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('User', userModel)
